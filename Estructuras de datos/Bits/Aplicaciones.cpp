f8e4cdfed801914667155716508936b79dc04f4d
Dusk1706 <dylanmigafi@outlook.com>
2023-02-06 14:28:47 -0700

    Aplicaciones Bits

---
+++ Estructuras de datos/Bits/Aplicaciones.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void parOImpar(int numero){
+    if(numero & 1){
+        cout<<"Es impar"<<endl;
+    }else{
+        cout<<"Es par"<<endl;
+    }
+}
+
+
+
+
+int main(){
+
+    parOImpar(39);
+
+
+
+    return 0;
+}
--- Estructuras de datos/Bits/OperadoresBasicos.cpp
+++ Estructuras de datos/Bits/OperadoresBasicos.cpp
@@ -50,5 +50,23 @@ int main(){
         ~ -6 = -(-6 + 1) = 5
     */
 
+    /* Desplazamiento de bits
+
+       Desplazamiento a la izquierda N << k 
+       Agrega k bits apagados a N.
+       Por ejemplo, 14 << 2 = 56
+       14 = 00001110
+       56 = 00111000
+       Es similar a multiplicar N por 2^k.
+
+       Desplazamiento a la derecha N >> k 
+       Elimina los últimos k bits de N.
+       Por ejemplo, 49 >> 3 = 6
+       49 = 00110001
+       6  = 00000110
+       Es similar a dividir x por 2k.
+    */
+
+
     return 0;
 }
