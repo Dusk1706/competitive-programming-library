ca50f54360992f7e088c5835b2aa8fb6ea8e4e1d
Dusk1706 <dylanmigafi@outlook.com>
2023-02-23 21:42:23 -0700

    Actualizado

--- Plantillas/temp_comp.cpp
+++ Plantillas/temp_comp.cpp
@@ -6,7 +6,8 @@ using namespace std;
 #define s second
 #define all(s) begin(s), end(s)
 #define rall(n) n.rbegin(), n.rend()
-#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define FOR(x, a, b) for(int x = a; x <= b; x++)
+#define FORR(x, a, b) for(int x = a; x >= b; x--)
 #define deb(x)      cerr << #x << " = " << x << '\n';
 #define deb2(x, y)  cerr << #x << " = " << x << ", " << #y << " = " << y << '\n';
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
