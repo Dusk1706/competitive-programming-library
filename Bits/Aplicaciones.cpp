fa73f8f792cc9bb6a198051a1b7b082b8409ec64
Dusk1706 <dylanmigafi@outlook.com>
2023-03-04 20:57:24 -0700

    Actualizacion

--- Bits/Aplicaciones.cpp
+++ Bits/Aplicaciones.cpp
@@ -3,44 +3,44 @@
 using namespace std;
 
 //Obtiene el bit en cierto indice base 0 para base 1 poner i-1
-int getBit(int n, int i){
-    return (n & (1 << i))>0?1:0;
-}
+int getBit(int n, int i){ return (n & (1 << i))>0?1:0; }
 
 //Apagar el bit en cierto indice base 0 para base 1 poner i-1
-void clearBit(int &n,int i){
-    n &= ~(1 << i);
-}
+void clearBit(int &n,int i){ n &= ~(1 << i); }
 
 //Prender el bit en cierto indice base 0 para base 1 poner i-1
-void setBit(int &n, int i){ 
-    n |= (1 << i);
-}
+void setBit(int &n, int i){ n |= (1 << i); }
 
 //Invierte el bit en cierto indice base 0 para base 1 poner i-1
-int toggleBit(int n, int i){
-    return (n ^ (1 << i));
-}
+void toggleBit(int &n, int i){ n=(n ^ (1 << i)); }
 
 //Borra los primeros bits pasado una longitud de bits a borrar
-void clearFirstBits(int &n,int length){
-    n&=(-1<<length);
-}
+void clearFirstBits(int &n,int length){ n&=(-1<<length); }
 
 //Borra los bits en el rango i a j con base 0 para base 1 poner i-1 y j-1
 void clearBitRange(int &n,int i,int j){
     int mask= ((~0)<<(j+1)) | ((1<<i)-1);
     n&=mask;
 }
+//Imprime el numero en binario
+void printSet(int n) {                        
+  printf("Numero = %2d = ", n);
+  stack<int> st;
+  while (n)
+    st.push(n%2), n /= 2;
+  while (!st.empty())                         
+    printf("%d", st.top()), st.pop();
+  printf("\n");
+}
 
 int main(){
     int n=5;
     cout<<getBit(n,2)<<endl;
-    clearBit(n,2);
-    cout<<getBit(n,2)<<endl;
-    cout<<toggleBit(n,2);
-    clearFirstBits(n,1);
+    // clearBit(n,2);
+    // cout<<getBit(n,2)<<endl;
+    toggleBit(n,3);
+    // clearFirstBits(n,1);
     cout<<n<<endl;
-
+    printSet(n);
     return 0;
 }
--- Plantillas/temp_normal.cpp
+++ Plantillas/temp_normal.cpp
@@ -5,15 +5,16 @@ using namespace std;
 #define s second
 #define all(s) begin(s), end(s)
 #define rall(n) n.rbegin(), n.rend()
-#define FOR(x, a, b) for(int x = a; x <= b; x++)
+#define FOR(x, b) for(int x = 0; x <b; x++)
+#define FORE(x, a, b) for(int x = a; x <= b; x++)
 #define FORR(x, a, b) for(int x = a; x >= b; x--)
 #define deb(x)      cerr << #x << " = " << x << '\n';
 #define deb2(x, y)  cerr << #x << " = " << x << ", " << #y << " = " << y << '\n';
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 typedef long long ll;
 typedef unsigned long long ull;
-typedef pair<ll,ll> ii;
-typedef vector<ll> vi;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
 ll n, m, q, k, l, r, x, y, z, ans=0;
 
 int main(){ _
