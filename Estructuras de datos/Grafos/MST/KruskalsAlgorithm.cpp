7a5be7b14db46f0f3c646f98bc31b580edbc3e70
Dusk1706 <dylanmigafi@outlook.com>
2023-03-16 21:55:22 -0700

    Update KruskalsAlgorithm.cpp

--- Estructuras de datos/Grafos/MST/KruskalsAlgorithm.cpp
+++ Estructuras de datos/Grafos/MST/KruskalsAlgorithm.cpp
@@ -1,14 +1,25 @@
 #include <bits/stdc++.h>
+//Pura gente del coach moy
 using namespace std;
+#define ENDL '\n'
+#define f first
+#define s second
+#define all(s) begin(s), end(s)
+#define rall(n) n.rbegin(), n.rend()
+#define FOR(x, b) for(int x = 0; x <b; x++)
+#define FORE(x, a, b) for(int x = a; x <= b; x++)
+#define FORR(x, a, b) for(int x = a; x >= b; x--)
+#define deb(x)      cerr << #x << " = " << x << '\n';
+#define deb2(x, y)  cerr << #x << " = " << x << ", " << #y << " = " << y << '\n';
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef tuple<int,int,int> T_iii;
+typedef tuple<int,int,int,int> T_iiii;
 
-#define P_ii pair<int,int>
-#define P_si pair<string,int>
-#define P_is pair<int,string>
-#define T_iii tuple<int,int,int>
-#define T_iiii tuple<int,int,int,int>
-#define vi vector<int>
-#define INF 1<<28
-#define ull unsigned long long
 
 class UnionFind {
 private:
