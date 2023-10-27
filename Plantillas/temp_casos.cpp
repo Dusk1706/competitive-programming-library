16d8a1c0a995e4fe37b08b3e5f7246a0f9e5098f
Dusk1706 <dylanmigafi@outlook.com>
2023-10-27 14:35:53 -0700

    Update temp_casos.cpp

--- Plantillas/temp_casos.cpp
+++ Plantillas/temp_casos.cpp
@@ -5,33 +5,22 @@ using namespace std;
 #define all(s) begin(s), end(s)
 #define rall(n) n.rbegin(), n.rend()
 #define FOR(x, b) for(int x = 0; x <b; x++)
-#define FORE(x, a, b) for(int x = a; x <= b; x++)
-#define FORR(x, a, b) for(int x = a; x >= b; x--)
-#define deb(x)      cerr << #x << " = " << x << '\n';
-#define deb2(x, y)  cerr << #x << " = " << x << ", " << #y << " = " << y << '\n';
-#define _ ios_base::sync_with_stdio(0);cin.tie(0);
 typedef long long ll;
-typedef unsigned long long ull;
 typedef pair<int,int> ii;
 typedef vector<int> vi;
 typedef vector<ii> vii;
-
-const ll MOD = 1e9+7, INF=1e18, template_array_size = 1e6;
-ll gcd(ll a, ll b){return (b?gcd(b,a%b):a);}
-ll lcm(ll a, ll b){ if(!a || !b) return 0; return a * b / gcd(a, b); }
-string s, t;
-ll a[template_array_size], b[template_array_size];
-ll n, m, q, k, l, r, x, y, z, ans=0;
+const ll MOD = 1e9+7, INF=1e18;
 
 void solve(int tc=0){
-    ans=0;
+    ll ans=0;
 
 
-    
+
     cout<<ans<<ENDL;
 }
 
-int main(){_
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
     int tc=1;
     // cin>>tc;
     for(int t=0;t<tc;t++){
