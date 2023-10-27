f4ca4960f18d0df240e25d9af047572109286857
Dusk1706 <dylanmigafi@outlook.com>
2023-10-27 14:34:15 -0700

    Update temp_normal.cpp

--- Plantillas/temp_normal.cpp
+++ Plantillas/temp_normal.cpp
@@ -5,20 +5,13 @@ using namespace std;
 #define all(s) begin(s), end(s)
 #define rall(n) n.rbegin(), n.rend()
 #define FOR(x, b) for(int x = 0; x <b; x++)
-#define FORE(x, a, b) for(int x = a; x <= b; x++)
-#define FORR(x, a, b) for(int x = a; x >= b; x--)
-#define deb(x)      cerr << #x << " = " << x << '\n';
-#define deb2(x, y)  cerr << #x << " = " << x << ", " << #y << " = " << y << '\n';
 typedef long long ll;
-typedef unsigned long long ull;
 typedef pair<int,int> ii;
 typedef vector<int> vi;
 typedef vector<ii> vii;
-const ll MOD = 1e9+7, INF=1e18, template_array_size = 1e6;
-ll n, m, q, k, l, r, x, y, z, ans=0;
-string s,t;
+const ll MOD = 1e9+7, INF=1e18;
 
-int main(){ 
+int main(){
   ios_base::sync_with_stdio(0);
   cin.tie(0);
 
