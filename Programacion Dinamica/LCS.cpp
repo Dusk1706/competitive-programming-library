67a55d4d2448a3abfdb53026ba4758ff45ca277f
Dusk1706 <dylanmigafi@outlook.com>
2024-01-13 14:01:46 -0700

    Update LCS.cpp

--- Programacion Dinamica/LCS.cpp
+++ Programacion Dinamica/LCS.cpp
@@ -13,12 +13,10 @@ int LongestCommonSubsequence(string &s, string &t){
   memset(dp, 0, sizeof(dp));
   int n = s.size(), m = t.size();
   
-  for(int i=0; i<=n; i++){
-    for(int j=0; j<=m; j++){
-      if(!i || !j){
-        dp[i][j]=0;
-      }else if(s[i-1] == t[j - 1]){
-        dp[i][j] = max({dp[i - 1][j - 1] + 1, dp[i - 1][j], dp[i][j - 1]});
+  for(int i=1; i<=n; i++){
+    for(int j=1; j<=m; j++){
+      if(s[i - 1] == t[j - 1]){
+        dp[i][j] = dp[i - 1][j - 1] + 1;
       }else{
         dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
       }
