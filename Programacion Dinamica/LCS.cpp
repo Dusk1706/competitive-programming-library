4f5c855ab1df91fa6002ffc8af6e6e5f279734b4
Dusk1706 <dylanmigafi@outlook.com>
2024-01-13 13:56:48 -0700

    Create LCS.cpp

---
+++ Programacion Dinamica/LCS.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+//Pura gente del coach moy
+using namespace std;
+#define ENDL '\n'
+#define FOR(x, b) for(int x = 0; x <b; x++)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+
+const int MAXN=1000 + 5;
+
+int dp[MAXN][MAXN];
+
+int LongestCommonSubsequence(string &s, string &t){
+  memset(dp, 0, sizeof(dp));
+  int n = s.size(), m = t.size();
+  
+  for(int i=0; i<=n; i++){
+    for(int j=0; j<=m; j++){
+      if(!i || !j){
+        dp[i][j]=0;
+      }else if(s[i-1] == t[j - 1]){
+        dp[i][j] = max({dp[i - 1][j - 1] + 1, dp[i - 1][j], dp[i][j - 1]});
+      }else{
+        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    } 
+  }
+  return dp[n][m];
+}
+
+int main(){ _
+  string s, t;
+  while(cin>>s && s!="#"){
+    cin>>t;
+    
+    int lcs = LongestCommonSubsequence(s,t);
+    cout<< lcs <<ENDL;
+  }
+
+
+  return 0;
+}
